aa318ffe7c0fedc9704aeb898965ab37d9c4157c
Jisara Wuttikreekiat <jwuttikr@uwaterloo.ca>
2026-02-26 00:35:04 -0500

    working text display

--- LCD_display/LCD3/src/main.cpp
+++ LCD_display/LCD3/src/main.cpp
@@ -1,18 +1,44 @@
 #include <Arduino.h>
+#include <Arduino_GFX_Library.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define LCD_BL 1  // try 2 or 38 if screen stays dark
+
+Arduino_DataBus *bus = new Arduino_ESP32QSPI(
+  45, 47, 21, 48, 40, 39
+);
+
+Arduino_GFX *panel = new Arduino_NV3041A(
+  bus, GFX_NOT_DEFINED, 1, true
+);
+
+Arduino_Canvas *gfx = new Arduino_Canvas(272, 480, panel);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  Serial.println("Booting...");
+
+  gfx->begin();
+  pinMode(LCD_BL, OUTPUT);
+  digitalWrite(LCD_BL, HIGH);
+  Serial.println("Display init done");
+
+  gfx->fillScreen(0x0000);           // black
+  gfx->setTextColor(0xFFFF);         // white
+  gfx->setTextSize(3);
+  gfx->setCursor(40, 200);
+  gfx->print("Hello!");
+  gfx->setCursor(20, 240);
+  gfx->setTextSize(2);
+  gfx->print("Screen is working");
+  gfx->flush();
+
+  Serial.println("Screen drawn");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  static uint32_t last = 0;
+  if (millis() - last >= 1000) {
+    last = millis();
+    Serial.printf("Uptime: %lu s\n", millis() / 1000);
+  }
 }
